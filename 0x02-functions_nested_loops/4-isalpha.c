3136a355b5912d75d7aba2164fba1f83a6c93363
Perpy93 <Perpetualchioma00@gmail.com>
2023-09-13 02:55:04 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,15 @@
 /**
  * _isalpha - checks for alphabets
  * @c: function parameter
- * Retrn: 1 and 0
+ * Return: 1 and 0
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
 
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+return (0);
 }
