dc7610ff64fea494bb444fe81618814d50804b4d
Perpy93 <Perpetualchioma00@gmail.com>
2023-09-13 03:45:24 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * _abs - function to find the absolute value of a number
- * @a:  function parameter
+ * @n: int to check
  * Return: the absolute value of int
  */
-
 int _abs(int n)
 {
-	if (n <= 0)
+	if (n >= 0)
 	{
 	return (n);
 	}
