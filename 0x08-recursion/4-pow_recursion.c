c2a531357dccc83fee79637c024fc658e323947f
Perpy93 <Perpetualchioma00@gmail.com>
2023-09-28 11:19:03 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - return value of x^y
+ * @x: power to raise
+ * @y: power to be raised to
+ * Return - result of the power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < O)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
