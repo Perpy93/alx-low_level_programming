6138c22e6ffd40a86346e789823976adfab50808
Perpy93 <Perpetualchioma00@gmail.com>
2023-10-01 15:13:09 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * prime_a - calculate if a prime number
+ * @a: number to be checked
+ * @b: a divisor
+ * Return : returns 0
+ */
+int prime_a(int a, int b)
+{
+	if (a <= 1 || (a != b && a % b == 0))
+	{
+		return (0);
+	}
+	else if (a == b)
+	{
+		return (1);
+	}
+	return (prime_a(a, b + 1));
+}
+/**
+ * is_prime_number - Check if number is a prime number
+ * @n: number to be checked
+ * Return: 0 for composite and 1 for prime
+ */
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
