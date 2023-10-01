dfe623663af3dcc98987d4c0b224bbc3acc7c3f5
Perpy93 <Perpetualchioma00@gmail.com>
2023-10-01 16:19:11 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion - return value of x^y
  * @x: power to raise
  * @y: power to be raised to
- * Return - result of the power
+ * Return: result of the power
  */
 int _pow_recursion(int x, int y)
 {
