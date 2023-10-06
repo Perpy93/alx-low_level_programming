b6939aaa85182044b14ba13ccb4a17e3b886c3a7
Perpy93 <Perpetualchioma00@gmail.com>
2023-10-06 05:16:00 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: input dest value
+ * @src: input source value
+ * @n: input a number
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (i = i; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function to find the absolute value of a number
+ * @n: int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	return (-n);
+}
