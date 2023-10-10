f05964b1f4088b1ac6a583beed2e02372c1b7a48
Perpy93 <Perpetualchioma00@gmail.com>
2023-10-10 04:40:28 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
