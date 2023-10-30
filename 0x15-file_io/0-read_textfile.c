af2613cf71c3ced88b9736cb7bfc91394a821846
Perpy93 <Perpetualchioma00@gmail.com>
2023-10-30 09:07:23 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * read_textfile - funcion that reads a text file and print
+ * @filename: filename to read
+ * @letters: number of  bytes to read
+ * Return: number of bytes read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buff;
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buff =  malloc(sizeof(char) *letters);
+	if (buff == NULL)
+		return (0);
+
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, buff, nrd);
+	close(fd);
+
+	return (nwr);
+}
