a26a5c386b133f25546eea8bdf6f9d274352a144
Perpy93 <Perpetualchioma00@gmail.com>
2023-12-07 05:56:00 -0800

    list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return the number of element in a 
+ * double linked list
+ *
+ * @h: pointer to head node
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	count = 0;
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL);
+	h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
