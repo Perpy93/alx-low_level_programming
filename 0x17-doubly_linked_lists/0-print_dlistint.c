040b5274c1aa62ebabb01c7d09e68e85acf588c9
Perpy93 <Perpetualchioma00@gmail.com>
2023-12-07 05:31:35 -0800

    print lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - prints doubly linked lists
+ *@h: address of head node
+ *
+ *Return: size of list
+ */
+
+print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Doubly linked list transversing in both forward and backward direction
