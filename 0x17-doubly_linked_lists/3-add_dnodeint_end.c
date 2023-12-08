50e1dcfdbd20078c79913e59b4fa18c9f361b808
Perpy93 <Perpetualchioma00@gmail.com>
2023-12-07 17:53:40 -0800

    add node at list end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,27 +9,33 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t)), *node;
+	dlistint_t *new_node, *tmp;
 
-	if (!head || !new)
-		return (new ? free(new), NULL : NULL);
+	if (head == NULL)
+		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	tmp = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
 	if (!*head)
-
 	{
-		new->prev = NULL;
-		*head = new;
+		*head = new_node;
+		new_node->prev = NULL;
+		return (new_node);
 	}
-	else
+	while (tmp && tmp->next)
 	{
-		node = *head;
-		while (node->next)
-			node = node->next;
-
-		node->next = new;
-		new->prev = node;
+		tmp = tmp->next;
 	}
-	return (new);
+
+	tmp->next = new_node;
+	new_node->prev = tmp;
+
+	return (new_node);
 }
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node in a doubly
+ *                            list at a given position.
+ * @h: pointer to the head of the dlistint_t list.
+ * @idx: position to insert a new node.
+ * @n: integer to contain the new node.
+ *
+ * Return: If function fails, NULL.
+ *        if not retturn address of the new node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *new, *h;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
+}
